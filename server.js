1058da4ac1d133631848c00fef11d1d8022798e1
keerthanaanandhan5859-web <keerthanaanandhan5859@gmail.com>
2026-08-29 14:21:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -357,7 +357,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 
     console.log(
-        `Server running on http://localhost:${PORT}`
+        `Server running on https://interview-project-uimv.onrender.com:${PORT}`
     );
 
 });
